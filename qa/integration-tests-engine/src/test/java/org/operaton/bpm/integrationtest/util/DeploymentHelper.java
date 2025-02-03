b9c10aeb22127736bf11bec63e2485078774bf99
Karsten Thoms <thoms@itemis.de>
2025-02-03 05:56:26 +0100

    rename constants CAMUNDA_XXX -> OPERATON_XXX

--- qa/integration-tests-engine/src/test/java/org/operaton/bpm/integrationtest/util/DeploymentHelper.java
+++ qa/integration-tests-engine/src/test/java/org/operaton/bpm/integrationtest/util/DeploymentHelper.java
@@ -22,24 +22,24 @@ import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 
 public class DeploymentHelper extends AbstractDeploymentHelper {
 
-  protected static final String CAMUNDA_EJB_CLIENT = "org.operaton.bpm.javaee:operaton-ejb-client-jakarta";
-  protected static final String CAMUNDA_ENGINE_CDI = "org.operaton.bpm:operaton-engine-cdi-jakarta";
-  protected static final String CAMUNDA_ENGINE_SPRING = "org.operaton.bpm:operaton-engine-spring-6";
+  protected static final String OPERATON_EJB_CLIENT = "org.operaton.bpm.javaee:operaton-ejb-client-jakarta";
+  protected static final String OPERATON_ENGINE_CDI = "org.operaton.bpm:operaton-engine-cdi-jakarta";
+  protected static final String OPERATON_ENGINE_SPRING = "org.operaton.bpm:operaton-engine-spring-6";
 
   public static JavaArchive getEjbClient() {
-    return getEjbClient(CAMUNDA_EJB_CLIENT);
+    return getEjbClient(OPERATON_EJB_CLIENT);
   }
 
   public static JavaArchive getEngineCdi() {
-    return getEngineCdi(CAMUNDA_ENGINE_CDI);
+    return getEngineCdi(OPERATON_ENGINE_CDI);
   }
 
   public static JavaArchive[] getWeld() {
-    return getWeld(CAMUNDA_ENGINE_CDI);
+    return getWeld(OPERATON_ENGINE_CDI);
   }
 
   public static JavaArchive[] getEngineSpring() {
-    return getEngineSpring(CAMUNDA_ENGINE_SPRING);
+    return getEngineSpring(OPERATON_ENGINE_SPRING);
   }
 
   protected static JavaArchive[] getWeld(String engineCdiArtifactName) {
