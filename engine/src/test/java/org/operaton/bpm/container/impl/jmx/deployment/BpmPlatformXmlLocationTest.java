7faabe31c05b0a630228cd735731599d5c0d23b8
Karsten Thoms <thoms@itemis.de>
2025-03-03 06:13:46 +0100

    mock jndi

--- engine/src/test/java/org/operaton/bpm/container/impl/jmx/deployment/BpmPlatformXmlLocationTest.java
+++ engine/src/test/java/org/operaton/bpm/container/impl/jmx/deployment/BpmPlatformXmlLocationTest.java
@@ -16,25 +16,23 @@
  */
 package org.operaton.bpm.container.impl.jmx.deployment;
 
+import org.operaton.bpm.container.impl.tomcat.deployment.TomcatParseBpmPlatformXmlStep;
+import static org.operaton.bpm.container.impl.deployment.AbstractParseBpmPlatformXmlStep.*;
+import static org.operaton.bpm.container.impl.tomcat.deployment.TomcatParseBpmPlatformXmlStep.CATALINA_HOME;
+
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.operaton.bpm.container.impl.tomcat.deployment.TomcatParseBpmPlatformXmlStep;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.operaton.bpm.container.impl.deployment.AbstractParseBpmPlatformXmlStep.BPM_PLATFORM_XML_FILE;
-import static org.operaton.bpm.container.impl.deployment.AbstractParseBpmPlatformXmlStep.BPM_PLATFORM_XML_LOCATION;
-import static org.operaton.bpm.container.impl.deployment.AbstractParseBpmPlatformXmlStep.BPM_PLATFORM_XML_SYSTEM_PROPERTY;
-import static org.operaton.bpm.container.impl.tomcat.deployment.TomcatParseBpmPlatformXmlStep.CATALINA_HOME;
+import static org.mockito.Mockito.when;
 
 /**
  * Checks the correct retrieval of bpm-platform.xml file through JNDI,
@@ -61,12 +59,12 @@ public class BpmPlatformXmlLocationTest {
   private static final String BPM_PLATFORM_XML_LOCATION_URL_HTTPS_PROTOCOL = "https://localhost:8080/operaton/" + BPM_PLATFORM_XML_FILE;
 
   @Rule
-  public MockInitialContextRule initialContextRule = new MockInitialContextRule(new InitialContext());
+  public MockInitialContextRule initialContextRule = new MockInitialContextRule();
 
-    public BpmPlatformXmlLocationTest() throws NamingException {
-    }
+  public BpmPlatformXmlLocationTest() throws NamingException {
+  }
 
-    @Test
+  @Test
   public void checkValidBpmPlatformXmlResourceLocationForUrl() throws MalformedURLException {
     TomcatParseBpmPlatformXmlStep tomcatParseBpmPlatformXmlStep = new TomcatParseBpmPlatformXmlStep();
 
@@ -121,11 +119,13 @@ public class BpmPlatformXmlLocationTest {
 
   @Test
   public void getBpmPlatformXmlLocationFromJndi() throws NamingException, MalformedURLException {
-    Context context = new InitialContext();
-    context.bind("java:comp/env/" + BPM_PLATFORM_XML_LOCATION, BPM_PLATFORM_XML_FILE_ABSOLUTE_LOCATION);
+    // given
+    when(initialContextRule.getMockedContext().lookup("java:comp/env/" + BPM_PLATFORM_XML_LOCATION)).thenReturn(BPM_PLATFORM_XML_FILE_ABSOLUTE_LOCATION);
 
+    // when
     URL url = new TomcatParseBpmPlatformXmlStep().lookupBpmPlatformXmlLocationFromJndi();
 
+    // then
     assertThat(url).isEqualTo(new File(BPM_PLATFORM_XML_FILE_ABSOLUTE_LOCATION).toURI().toURL());
   }
 
@@ -184,14 +184,16 @@ public class BpmPlatformXmlLocationTest {
 
   @Test
   public void lookupBpmPlatformXml() throws NamingException, MalformedURLException {
-    Context context = new InitialContext();
-    context.bind("java:comp/env/" + BPM_PLATFORM_XML_LOCATION, BPM_PLATFORM_XML_FILE_ABSOLUTE_LOCATION);
+    // given
+    when(initialContextRule.getMockedContext().lookup("java:comp/env/" + BPM_PLATFORM_XML_LOCATION)).thenReturn(BPM_PLATFORM_XML_FILE_ABSOLUTE_LOCATION);
 
+    // when
     URL url = new TomcatParseBpmPlatformXmlStep().lookupBpmPlatformXml();
 
+    // then
     assertThat(url).isEqualTo(new File(BPM_PLATFORM_XML_FILE_ABSOLUTE_LOCATION).toURI().toURL());
   }
-  
+
   private static String getBpmPlatformXmlLocationParentDir() {
     String baseDir = BpmPlatformXmlLocationTest.class.getProtectionDomain().getCodeSource().getLocation().getFile();
     try {
